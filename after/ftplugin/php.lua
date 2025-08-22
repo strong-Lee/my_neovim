9e54dd281b83d16f130472d8d0233edcc3236f89
Cay <apple@192.168.1.220>
2025-08-22 21:19:10 +0800

    php4个空格格式化

--- after/ftplugin/php.lua
+++ after/ftplugin/php.lua
@@ -5,4 +5,4 @@ vim.bo.tabstop = 4
 vim.bo.softtabstop = 4
 vim.bo.shiftwidth = 4
 
-print("PHP ftplugin loaded: Indent set to 4 spaces.") -- (可选) 这是一个调试信息，确认文件被加载了。配置好后可以删除。
+-- print("PHP ftplugin loaded: Indent set to 4 spaces.") -- (可选) 这是一个调试信息，确认文件被加载了。配置好后可以删除。
